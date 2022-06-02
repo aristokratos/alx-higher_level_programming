4ae25a993eff3ce9a871597f62051affffdb96f1
aristokratos <stefaneey@hmail.com>
2022-06-02 08:37:39 -0700

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if title == "main":
+if _title_ == "_main_":
     from 0_add import add
     a = 1
     b = 2
-    print("{}+{}={}".format(a,b,add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
